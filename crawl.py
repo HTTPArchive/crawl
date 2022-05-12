0fc7f4efa93b65d44a415a45bddcd6489a828d95
Patrick Meenan <PatMeenan@gmail.com>
2022-05-12 13:51:59 -0700

    Increased to 10 messages

--- crawl.py
+++ crawl.py
@@ -83,15 +83,15 @@ class Crawl(object):
             from google.cloud import pubsub_v1
             retry_subscriber = pubsub_v1.SubscriberClient()
             retry_subscription = retry_subscriber.subscription_path(self.project, self.retry_queue)
-            retry_flow_control = pubsub_v1.types.FlowControl(max_messages=1)
+            retry_flow_control = pubsub_v1.types.FlowControl(max_messages=10)
             retry_future = retry_subscriber.subscribe(retry_subscription, callback=self.retry_job, flow_control=retry_flow_control, await_callbacks_on_shutdown=True)
             failed_subscriber = pubsub_v1.SubscriberClient()
             failed_subscription = failed_subscriber.subscription_path(self.project, self.failed_queue)
-            failed_flow_control = pubsub_v1.types.FlowControl(max_messages=1)
+            failed_flow_control = pubsub_v1.types.FlowControl(max_messages=10)
             failed_future = failed_subscriber.subscribe(failed_subscription, callback=self.retry_job, flow_control=failed_flow_control, await_callbacks_on_shutdown=True)
             completed_subscriber = pubsub_v1.SubscriberClient()
             completed_subscription = completed_subscriber.subscription_path(self.project, self.completed_queue)
-            completed_flow_control = pubsub_v1.types.FlowControl(max_messages=1)
+            completed_flow_control = pubsub_v1.types.FlowControl(max_messages=10)
             completed_future = completed_subscriber.subscribe(completed_subscription, callback=self.crawl_job, flow_control=completed_flow_control, await_callbacks_on_shutdown=True)
 
             # Pump jobs for 4 minutes
