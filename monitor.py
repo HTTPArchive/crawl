d8be4d1d1a3624a22a810179a3d9a8ac0ca8fb9a
Patrick Meenan <pmeenan@chromium.org>
2024-03-29 15:27:10 -0400

    Upload metrics

--- monitor.py
+++ monitor.py
@@ -18,20 +18,54 @@ class Monitor(object):
 
     def run(self):
         """ Update the metrics every 30 seconds """
+        import requests
+        metadata_server = "http://metadata/computeMetadata/v1/instance/"
+        metadata_flavor = {'Metadata-Flavor' : 'Google'}
+        gce_id = requests.get(metadata_server + 'id', headers = metadata_flavor).text
+        gce_zone = requests.get(metadata_server + 'zone', headers = metadata_flavor).text
+        logging.debug("GCE instance: %s, zone: %s", gce_id, gce_zone)
+
         while True:
             counts = {}
+            for tube in TUBES:
+                counts[tube] = 0
             try:
                 beanstalk = greenstalk.Client(('127.0.0.1', 11300))
                 for tube in beanstalk.tubes():
                     try:
                         stats = beanstalk.stats_tube(tube)
-                        logging.debug(json.dumps(stats))
-                        #if tube in TUBES:
-                        #    counts[tube]
+                        count = stats['current-jobs-ready'] + stats['current-jobs-reserved']
+                        logging.debug('%s: %d', tube, count)
+                        if tube in TUBES:
+                            counts[tube] = count
                     except Exception:
                         pass
             except Exception:
                 logging.exception("Error collecting metrics")
+            try:
+                client = monitoring_v3.MetricServiceClient()
+                project_name = "projects/httparchive"
+                values = []
+                for tube in TUBES:
+                    series = monitoring_v3.TimeSeries()
+                    series.metric.type = "custom.googleapis.com/crawl/queue/{}".format(tube)
+                    series.resource.type = "gce_instance"
+                    series.resource.labels["instance_id"] = gce_id
+                    series.resource.labels["zone"] = gce_zone
+                    series.metric.labels["Queue"] = tube
+                    now = time.time()
+                    seconds = int(now)
+                    nanos = int((now - seconds) * 10**9)
+                    interval = monitoring_v3.TimeInterval(
+                        {"end_time": {"seconds": seconds, "nanos": nanos}}
+                    )
+                    point = monitoring_v3.Point({"interval": interval, "value": {"int64Value": str(counts[tube])}})
+                    series.points = [point]
+                    values.append(series)
+                logging.debug(json.dumps(values))
+                client.create_time_series(name=project_name, time_series=values)
+            except Exception:
+                logging.exception("Error reporting metrics")
             time.sleep(30)
 
 # Make sure only one instance is running at a time
