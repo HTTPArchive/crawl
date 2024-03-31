dff13aba4b6855df53176c83f9959db90390f9c0
Patrick Meenan <PatMeenan@gmail.com>
2024-03-31 21:19:53 +0000

    Improved the draining to be more targeted

--- drain.py
+++ drain.py
@@ -2,7 +2,6 @@
 # Copyright 2024 Google Inc.
 import greenstalk
 import logging
-from google.cloud import monitoring_v3
 
 TUBES = {'crawl': 'Crawl tests work queue',
          'retry': 'Crawl tests retry queue',
@@ -14,23 +13,24 @@ def main():
     beanstalk = greenstalk.Client(('127.0.0.1', 11300), encoding=None)
     previous = None
     for tube in beanstalk.tubes():
-        beanstalk.watch(tube)
-        if previous is not None:
-            beanstalk.ignore(previous)
-        previous = tube
-        count = 0
-        try:
-            while True:
-                job = beanstalk.reserve(5)
-                count += 1
-                if count % 10000 == 0:
-                    logging.info("Drained %d jobs from %s so far...", count, tube)
-                beanstalk.delete(job)
-        except greenstalk.TimedOutError:
-            pass
-        except Exception:
-            logging.exception("Error draining tube %s", tube)
-        logging.info('Drained %d jobs from %s', count, tube)
+        if tube in TUBES:
+            beanstalk.watch(tube)
+            if previous is not None:
+                beanstalk.ignore(previous)
+            previous = tube
+            count = 0
+            try:
+                while True:
+                    job = beanstalk.reserve(1)
+                    count += 1
+                    if count % 10000 == 0:
+                        logging.info("Drained %d jobs from %s so far...", count, tube)
+                    beanstalk.delete(job)
+            except greenstalk.TimedOutError:
+                pass
+            except Exception:
+                logging.exception("Error draining tube %s", tube)
+            logging.info('Drained %d jobs from %s', count, tube)
 
 if __name__ == '__main__':
     logging.basicConfig(
