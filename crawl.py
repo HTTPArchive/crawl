db10ef327d6987d475097e548d61c8ea5c07f1b5
Patrick Meenan <pmeenan@chromium.org>
2024-12-07 14:12:44 -0500

    Switched back to prod

--- crawl.py
+++ crawl.py
@@ -26,8 +26,8 @@ MAX_DEPTH = 1
 MAX_BREADTH = 1
 STATUS_DIRTY = False
 
-TESTING = True
-LIMIT_TESTS = True
+TESTING = False
+LIMIT_TESTS = False
 
 class Crawl(object):
     """Main agent workflow"""
